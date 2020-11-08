a31f36a11fa1db153aefa257d65cf89acac53ad3
taidana97 <sieuquay199x@gmail.com>
2020-11-08 15:18:40 +0700

    first commit

---
+++ Fllappy/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Fllappy
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tmrCot = new System.Windows.Forms.Timer(this.components);
+            this.tmrDat = new System.Windows.Forms.Timer(this.components);
+            this.tmrThua = new System.Windows.Forms.Timer(this.components);
+            this.tmrXuatCot = new System.Windows.Forms.Timer(this.components);
+            this.tmrChayHinhBird = new System.Windows.Forms.Timer(this.components);
+            this.tmrRoi = new System.Windows.Forms.Timer(this.components);
+            this.tmrXoaCot = new System.Windows.Forms.Timer(this.components);
+            this.tmrDiem = new System.Windows.Forms.Timer(this.components);
+            this.tmrChimRoiKhiThua = new System.Windows.Forms.Timer(this.components);
+            this.plMain = new System.Windows.Forms.Panel();
+            this.plScore = new System.Windows.Forms.Panel();
+            this.lbBest = new System.Windows.Forms.Label();
+            this.labScore = new System.Windows.Forms.Label();
+            this.picHuongDan = new System.Windows.Forms.PictureBox();
+            this.picPlay = new System.Windows.Forms.PictureBox();
+            this.ptbChim = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.plMain.SuspendLayout();
+            this.plScore.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picHuongDan)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlay)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbChim)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tmrCot
+            // 
+            this.tmrCot.Interval = 49;
+            this.tmrCot.Tick += new System.EventHandler(this.tmrCot_Tick);
+            // 
+            // tmrDat
+            // 
+            this.tmrDat.Enabled = true;
+            this.tmrDat.Tick += new System.EventHandler(this.tmrDat_Tick);
+            // 
+            // tmrThua
+            // 
+            this.tmrThua.Enabled = true;
+            this.tmrThua.Tick += new System.EventHandler(this.tmrThua_Tick);
+            // 
+            // tmrXuatCot
+            // 
+            this.tmrXuatCot.Interval = 6;
+            this.tmrXuatCot.Tick += new System.EventHandler(this.tmrXuatCot_Tick);
+            // 
+            // tmrChayHinhBird
+            // 
+            this.tmrChayHinhBird.Enabled = true;
+            this.tmrChayHinhBird.Tick += new System.EventHandler(this.tmrChay_Tick);
+            // 
+            // tmrRoi
+            // 
+            this.tmrRoi.Tick += new System.EventHandler(this.tmrRoi_Tick);
+            // 
+            // tmrXoaCot
+            // 
+            this.tmrXoaCot.Tick += new System.EventHandler(this.tmrXoaCot_Tick);
+            // 
+            // tmrDiem
+            // 
+            this.tmrDiem.Tick += new System.EventHandler(this.tmrDiem_Tick);
+            // 
+            // tmrChimRoiKhiThua
+            // 
+            this.tmrChimRoiKhiThua.Interval = 1;
+            this.tmrChimRoiKhiThua.Tick += new System.EventHandler(this.tmrChimRoiKhiThua_Tick);
+            // 
+            // plMain
+            // 
+            this.plMain.BackColor = System.Drawing.SystemColors.Control;
+            this.plMain.BackgroundImage = global::Fllappy.Properties.Resources.anh;
+            this.plMain.Controls.Add(this.plScore);
+            this.plMain.Controls.Add(this.picHuongDan);
+            this.plMain.Controls.Add(this.picPlay);
+            this.plMain.Controls.Add(this.ptbChim);
+            this.plMain.Controls.Add(this.pictureBox3);
+            this.plMain.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.plMain.Location = new System.Drawing.Point(0, 0);
+            this.plMain.Name = "plMain";
+            this.plMain.Size = new System.Drawing.Size(814, 499);
+            this.plMain.TabIndex = 0;
+            this.plMain.Paint += new System.Windows.Forms.PaintEventHandler(this.plMain_Paint);
+            this.plMain.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.plMain_PreviewKeyDown);
+            // 
+            // plScore
+            // 
+            this.plScore.BackColor = System.Drawing.Color.Transparent;
+            this.plScore.BackgroundImage = global::Fllappy.Properties.Resources.socre;
+            this.plScore.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.plScore.Controls.Add(this.lbBest);
+            this.plScore.Controls.Add(this.labScore);
+            this.plScore.Location = new System.Drawing.Point(287, 151);
+            this.plScore.Name = "plScore";
+            this.plScore.Size = new System.Drawing.Size(214, 120);
+            this.plScore.TabIndex = 6;
+            // 
+            // lbBest
+            // 
+            this.lbBest.AutoSize = true;
+            this.lbBest.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbBest.Location = new System.Drawing.Point(172, 74);
+            this.lbBest.Name = "lbBest";
+            this.lbBest.Size = new System.Drawing.Size(16, 16);
+            this.lbBest.TabIndex = 0;
+            this.lbBest.Text = "0";
+            // 
+            // labScore
+            // 
+            this.labScore.AutoSize = true;
+            this.labScore.Font = new System.Drawing.Font("Tahoma", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.labScore.Location = new System.Drawing.Point(172, 36);
+            this.labScore.Name = "labScore";
+            this.labScore.Size = new System.Drawing.Size(16, 16);
+            this.labScore.TabIndex = 0;
+            this.labScore.Text = "0";
+            // 
+            // picHuongDan
+            // 
+            this.picHuongDan.BackColor = System.Drawing.Color.Transparent;
+            this.picHuongDan.BackgroundImage = global::Fllappy.Properties.Resources.HuongDan1;
+            this.picHuongDan.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.picHuongDan.ErrorImage = null;
+            this.picHuongDan.Location = new System.Drawing.Point(99, 123);
+            this.picHuongDan.Name = "picHuongDan";
+            this.picHuongDan.Size = new System.Drawing.Size(119, 90);
+            this.picHuongDan.TabIndex = 5;
+            this.picHuongDan.TabStop = false;
+            // 
+            // picPlay
+            // 
+            this.picPlay.BackColor = System.Drawing.Color.Transparent;
+            this.picPlay.BackgroundImage = global::Fllappy.Properties.Resources.playbtn;
+            this.picPlay.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.picPlay.Location = new System.Drawing.Point(344, 289);
+            this.picPlay.Name = "picPlay";
+            this.picPlay.Size = new System.Drawing.Size(100, 50);
+            this.picPlay.TabIndex = 4;
+            this.picPlay.TabStop = false;
+            this.picPlay.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // ptbChim
+            // 
+            this.ptbChim.BackColor = System.Drawing.Color.Transparent;
+            this.ptbChim.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ptbChim.Location = new System.Drawing.Point(35, 172);
+            this.ptbChim.Name = "ptbChim";
+            this.ptbChim.Size = new System.Drawing.Size(58, 41);
+            this.ptbChim.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ptbChim.TabIndex = 3;
+            this.ptbChim.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(0, 380);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(920, 140);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 1;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(814, 499);
+            this.Controls.Add(this.plMain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.plMain.ResumeLayout(false);
+            this.plScore.ResumeLayout(false);
+            this.plScore.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picHuongDan)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picPlay)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbChim)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer tmrCot;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Timer tmrDat;
+        private System.Windows.Forms.PictureBox ptbChim;
+        private System.Windows.Forms.Timer tmrThua;
+        private System.Windows.Forms.Timer tmrXuatCot;
+        private System.Windows.Forms.Panel plMain;
+        private System.Windows.Forms.Timer tmrChayHinhBird;
+        private System.Windows.Forms.Timer tmrRoi;
+        private System.Windows.Forms.Timer tmrXoaCot;
+        private System.Windows.Forms.PictureBox picPlay;
+        private System.Windows.Forms.Timer tmrDiem;
+        private System.Windows.Forms.PictureBox picHuongDan;
+        private System.Windows.Forms.Panel plScore;
+        private System.Windows.Forms.Label lbBest;
+        private System.Windows.Forms.Label labScore;
+        private System.Windows.Forms.Timer tmrChimRoiKhiThua;
+    }
+}
+
---
+++ Fllappy/Form1.cs
@@ -0,0 +1,435 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Drawing.Text;
+
+namespace Fllappy
+{
+
+    public partial class Form1 : Form
+    {
+
+        bool chimBay = false;
+        int demchim = 0;
+        int demLoadChim = 0;
+        bool demthua = false;
+        bool demstart = false;
+        bool choilai = false;
+        int diem = 0;
+        Random t = new Random();
+        int m;
+        Point pointChim;
+        List<PictureBox> taocottren = new List<PictureBox>();
+        List<PictureBox> taocotduoi = new List<PictureBox>();
+        List<PictureBox> refresh_over = new List<PictureBox>();
+        Label lb = new Label();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pointChim = new Point(ptbChim.Location.X,ptbChim.Location.Y);
+            
+            this.plMain.Controls.Remove(plScore);
+
+            PrivateFontCollection pfc = new PrivateFontCollection();
+            pfc.AddFontFile(Path.Combine(Application.StartupPath, "04B_19__.TTF"));
+            lb.Font = new Font(pfc.Families[0], 18, FontStyle.Regular);
+            lb.BackColor = Color.Transparent;
+            lb.Location = new Point(389, 41);
+            this.plMain.Controls.Add(this.lb);
+        }
+        void diemchay(int n)
+        {
+            
+            lb.Width = 35;
+            lb.Height = 20;
+            lb.Text = n.ToString();
+            this.lb.BringToFront();
+        }
+        void refresh()
+        {
+            demchim = 0;
+            demthua = false;
+            //demstart = false;
+            choilai = false;
+            diem = 0;
+            this.lb.ResetText();
+        }
+        void Start()
+        {
+            
+            //this.tmrChay.Start();
+            this.tmrCot.Start();
+            this.tmrXuatCot.Start();
+            //this.tmrXoaCot.Start();
+            this.tmrRoi.Start();
+            this.tmrDat.Start();
+            this.tmrThua.Start();
+            this.tmrDiem.Start();
+            demstart = true;
+            this.plMain.Controls.Remove(picHuongDan);
+            this.plMain.Controls.Remove(plScore);
+        }
+        void GameOver()
+        {
+            tmrCot.Stop();
+            tmrDat.Stop();
+            tmrRoi.Stop();
+            //tmrXoaCot.Stop();
+            tmrXuatCot.Stop();
+            tmrThua.Stop();
+            tmrDiem.Stop();
+            choilai = true;
+            int diembest = Int32.Parse(lbBest.Text);
+            if (diembest <= diem)
+            {
+                lbBest.Text = diem.ToString();
+            }
+            labScore.Text = lb.Text;
+            this.plMain.Controls.Add(plScore);
+            plScore.BringToFront();
+        }
+        void songoku(int t)
+        {
+            switch (t)
+            {
+                case 1:
+                    ptbChim.BackgroundImage = Properties.Resources.c0;
+                    break;
+                case 2:
+                    ptbChim.BackgroundImage = Properties.Resources.c1;
+                    break;
+                case 3:
+                    ptbChim.BackgroundImage = Properties.Resources.c2;
+                    break;
+                case 4:
+                    ptbChim.BackgroundImage = Properties.Resources.c3;
+                    break;
+                case 5:
+                    ptbChim.BackgroundImage = Properties.Resources.c4;
+                    break;
+                case 6:
+                    ptbChim.BackgroundImage = Properties.Resources.c5;
+                    break;
+            }
+        }
+        void ChimBay(int t)
+        {
+            switch (t)
+            {
+                case 1:
+                    ptbChim.BackgroundImage = Properties.Resources.c6;
+                    break;
+                case 2:
+                    ptbChim.BackgroundImage = Properties.Resources.c7;
+                    break;
+                case 3:
+                    ptbChim.BackgroundImage = Properties.Resources.c8;
+                    break;
+                case 4:
+                    ptbChim.BackgroundImage = Properties.Resources.c9;
+                    break;
+            }
+        }
+        void LoadChim(int t)
+        {
+            switch (t)
+            {
+                case 1:
+                    ptbChim.BackgroundImage = Properties.Resources.a1;
+                    break;
+                case 2:
+                    ptbChim.BackgroundImage = Properties.Resources.a2;
+                    break;
+                case 3:
+                    ptbChim.BackgroundImage = Properties.Resources.a3;
+                    break;
+                case 4:
+                    ptbChim.BackgroundImage = Properties.Resources.a1;
+                    break;
+                case 5:
+                    ptbChim.BackgroundImage = Properties.Resources.a2;
+                    break;
+                case 6:
+                    ptbChim.BackgroundImage = Properties.Resources.a3;
+                    break;
+                case 7:
+                    ptbChim.BackgroundImage = Properties.Resources.a1;
+                    break;
+                case 8:
+                    ptbChim.BackgroundImage = Properties.Resources.a2;
+                    break;
+                case 9:
+                    ptbChim.BackgroundImage = Properties.Resources.a3;
+                    break;
+                case 10:
+                    ptbChim.BackgroundImage = Properties.Resources.a1;
+                    break;
+            }
+        }
+        void xuatcottren()
+        {
+            PictureBox cottren = new PictureBox();            
+            cottren.BackgroundImageLayout = ImageLayout.Stretch;            
+            cottren.BackgroundImage = Properties.Resources._4;
+            cottren.Width = 58;
+            cottren.Height = 335;
+            cottren.BackColor = Color.Transparent;           
+            cottren.Location = new Point(plMain.Size.Width - 1, -205 + m);           
+            taocottren.Add(cottren);
+            plMain.Controls.Add(cottren);            
+        }
+        void xuatcoiduoi()
+        {
+            PictureBox cotduoi = new PictureBox();
+            cotduoi.BackgroundImageLayout = ImageLayout.Stretch;
+            cotduoi.BackgroundImage = Properties.Resources._3;
+            cotduoi.Width = 58;
+            cotduoi.Height = 335;
+            cotduoi.BackColor = Color.Transparent;
+            cotduoi.Location = new Point(plMain.Size.Width - 1, 260 + m);
+            taocotduoi.Add(cotduoi);
+            plMain.Controls.Add(cotduoi);
+        }
+        void xoacot()
+        {
+            for (int i = 0; i < taocottren.Count; i++)
+            {
+                if (taocottren[i].Location.X + taocottren[i].Size.Width <= 0)
+                {
+                    plMain.Controls.Remove(taocottren[i]);
+                    taocottren.RemoveAt(i);
+                    plMain.Controls.Remove(taocotduoi[i]);
+                    taocotduoi.RemoveAt(i);                  
+
+                }
+            }            
+        }
+        void refreshcot()
+        {
+            for (int i = 0; i < taocottren.Count; i++)
+            {
+                taocottren.RemoveAt(i);
+            }
+            for (int i = 0; i < taocotduoi.Count; i++)
+            {
+                taocotduoi.RemoveAt(i);
+            }
+        }
+        void xoaanh()
+        {
+            foreach (PictureBox xoaanh in taocottren)
+            {
+                xoaanh.Dispose();
+            }
+            foreach (PictureBox xoaanh in taocotduoi)
+            {
+                xoaanh.Dispose();
+            }
+            foreach (PictureBox xoaanh in refresh_over)
+            {
+                xoaanh.Dispose();
+            }
+            refreshcot();
+        }
+        void hinhgameover()
+        {
+            PictureBox gameover = new PictureBox();            
+            gameover.BackgroundImageLayout = ImageLayout.Stretch;
+            gameover.BackgroundImage = Properties.Resources.GameOver;            
+            gameover.Width = 152;
+            gameover.Height = 60;
+            gameover.BackColor = Color.Transparent;
+            gameover.Location = new Point(320, 94);            
+            refresh_over.Add(gameover);
+            plMain.Controls.Add(gameover);
+            gameover.BringToFront();
+        }
+        private void plMain_Paint(object sender, PaintEventArgs e)
+        {
+            this.plMain.Focus();
+        }        
+        private void tmrCot_Tick(object sender, EventArgs e)
+        {
+            tmrCot.Interval = 100;
+            foreach (PictureBox item in taocottren)
+            {
+                item.Location = new Point(item.Location.X - 10, item.Location.Y);
+            }
+            foreach (PictureBox item1 in taocotduoi)
+            {
+                item1.Location = new Point(item1.Location.X - 10, item1.Location.Y);
+            }
+            this.plMain.Refresh();
+        }
+        private void tmrDat_Tick(object sender, EventArgs e)
+        {
+            tmrDat.Interval = 100;        
+            pictureBox3.Image = Properties.Resources._Dat1;
+            Point toado = new Point(pictureBox3.Location.X, pictureBox3.Location.Y);
+            toado.X -= 10;
+            if (toado.X < -50)
+            {
+                toado.X = 0;
+            }
+            pictureBox3.Location = toado;
+            xoacot();
+
+        }     
+        private void plMain_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (demstart == true && demthua == false && pointChim.Y > 15)
+            {
+                switch (e.KeyCode)
+                {
+                    case Keys.Space:
+                        pointChim.Y -= 40;
+                        chimBay = true;
+                        demchim = 0;
+                        break;
+                    default:
+                        break;
+                }
+            }           
+            ptbChim.Location = pointChim;
+            this.plMain.Refresh();
+        }
+        private void tmrThua_Tick(object sender, EventArgs e)
+        {
+            foreach (PictureBox item in taocottren)
+            {
+                if (pointChim.X >= item.Location.X - ptbChim.Size.Width - 5 && pointChim.Y <= item.Location.Y + item.Size.Height && pointChim.X <= item.Location.X + item.Size.Width)
+                {
+                    demthua = true;
+                }
+            }
+            foreach (PictureBox item1 in taocotduoi)
+            {
+                if (pointChim.X >= item1.Location.X - ptbChim.Size.Width - 5 && pointChim.Y >= item1.Location.Y - ptbChim.Size.Height && pointChim.X <= item1.Location.X + item1.Size.Width)
+                {
+                    demthua = true;
+                }
+            }
+            if (demthua == true)
+            {
+                GameOver();
+                tmrChimRoiKhiThua.Start();
+                this.plMain.Controls.Add(picPlay);
+                this.plMain.Controls.Remove(lb);
+                hinhgameover();                           
+            }
+            this.picPlay.BringToFront();
+
+        }
+        private void tmrChay_Tick(object sender, EventArgs e)
+        {            
+            if (demstart ==false)
+            {
+                demLoadChim++;
+                LoadChim(demLoadChim);
+                if (demLoadChim == 10)
+                {
+                    demLoadChim = 1;
+                }
+            }
+            else
+            {
+                demchim++;
+                if (chimBay)
+                {
+                    ChimBay(demchim);
+                }
+                else
+                {
+                    songoku(demchim);
+                }
+                if (demchim == 4 && chimBay == true)
+                {
+                    demchim = 1;
+                    chimBay = false;
+                }
+            }
+        }
+        private void tmrRoi_Tick(object sender, EventArgs e)
+        {
+            tmrRoi.Interval = 100;
+            pointChim.Y += 10;
+            if (pointChim.Y > 335 )
+            {
+                tmrRoi.Stop();
+                demthua = true;
+            }
+            ptbChim.Location = pointChim;
+        }
+        private void tmrXuatCot_Tick(object sender, EventArgs e)
+        {
+            m = t.Next(-70, 70);
+            tmrXuatCot.Interval = 3500;            
+          
+            xuatcottren();
+            xuatcoiduoi();             
+        }
+        private void tmrXoaCot_Tick(object sender, EventArgs e)
+        {
+            //xoacot();
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            Start();
+            this.plMain.Controls.Remove(picPlay);
+            if (choilai == true)
+            {
+                pointChim.X = 35;
+                pointChim.Y = 172;                
+                refresh();
+                xoaanh();
+                refreshcot();
+                this.plMain.Controls.Add(lb);
+                taocottren.Clear();
+                taocotduoi.Clear();
+            }
+            ptbChim.Location = pointChim;
+        }         
+        private void tmrDiem_Tick(object sender, EventArgs e)
+        {
+            tmrDiem.Interval = 80;
+            foreach (PictureBox item in taocottren)
+            {
+                if (pointChim.X >= item.Location.X + item.Size.Width - 10 && pointChim.Y >= item.Location.Y + item.Size.Height + 5 && pointChim.X <= item.Location.X + item.Size.Width)
+                {
+                    foreach (PictureBox item1 in taocotduoi)
+                    {
+                        if (pointChim.X >= item1.Location.X + item1.Size.Width - 10 && pointChim.Y <= item1.Location.Y - ptbChim.Size.Height - 5 && pointChim.X <= item1.Location.X + item1.Size.Width)
+                        {
+                            diem++;
+                        }
+                    }
+                }
+            }
+            diemchay(diem);
+        }        
+        private void tmrChimRoiKhiThua_Tick(object sender, EventArgs e)
+        {
+            
+            if (pointChim.Y <= 380 - ptbChim.Size.Height)
+            {
+                pointChim.Y += 7;
+                ptbChim.Location = pointChim;
+            }
+            else
+            {
+                tmrChimRoiKhiThua.Stop();
+            }
+        }
+    }
+}
